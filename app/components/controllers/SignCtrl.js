172a582b76fe78af47d1fbd677a46392961b7b3d
andreygaponik <andreygaponik@gmail.com>
2016-06-22 19:06:23 +0300

    push SignCtrl.js

---
+++ app/components/controllers/SignCtrl.js
@@ -0,0 +1,22 @@
+var SignCtrl = function($scope, $rootScope, $firebaseAuth, $location, Auth) {
+
+	// $scope.logIn = Auth.logIn;
+
+	var auth = $firebaseAuth();
+
+	$scope.logIn = function() {
+		auth
+		.$signInWithEmailAndPassword($scope.email, $scope.password)
+		.then(function(response) {
+			console.log(response);
+			$location.path('/home');
+		})
+		.catch(function(error) {
+		  console.log(error);
+		});
+	};
+};
+
+SignCtrl.$inject = ['$scope', '$rootScope', '$firebaseAuth', '$location', 'Auth'];
+app.controller('SignCtrl', SignCtrl);
+
